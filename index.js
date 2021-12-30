328aaf91ba37451e7c0b1729ca6a252111d2be70
RuthTadesse <tib6297@gmail.com>
2021-12-29 19:45:12 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  function introductionWithLanguageOptional(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+    function language("language");
+    function name ("name");
+  }
